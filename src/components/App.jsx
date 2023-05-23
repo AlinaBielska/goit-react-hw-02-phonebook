2bf5fcf6553be3e5eba5355d46980a7c9300108b
AlinaBielska <alinkabelka5@gmail.com>
2023-05-23 22:18:10 +0200

    krok 1 - aktualizowanie state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,29 +7,36 @@ export class App extends Component {
     name: ''
   }
 
-  submitContact = evt => {
+  onInputChange = evt => {
     this.setState({ name: evt.target.value });
+  }
+  onSubmitContact = evt => {
+    evt.preventDefault();
+    const { name, contacts } = this.state;
+    this.setState({ contacts: [...contacts, name] });
   }
 
   render() {
     const { contacts } = this.state;
     const name = this.state;
-    const submitContact = this.submitContact;
+    const onInputChange = this.onInputChange;
+    const onSubmitContact = this.onSubmitContact;
 
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmitContact}>
         <label>
           Name
           <input
             type="text"
             name="name"
             value={name}
+            onChange={onInputChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required />
         </label>
-        <button type="submit" onSubmit={submitContact}>Add contact</button>
+        <button type="submit">Add contact</button>
       </form>
       <ul>Contacts</ul>
     </div>
