88a66baa9b7d62c1e6facafa2ab5bd647f639193
AlinaBielska <alinkabelka5@gmail.com>
2023-05-23 21:51:31 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
+import React, { Component } from "react";
+
+
+export class App extends Component { 
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  submitContact = evt => {
+    this.setState({ name: evt.target.value });
+  }
+
+  render() {
+    const { contacts } = this.state;
+    const name = this.state;
+    const submitContact = this.submitContact;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <form>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required />
+        </label>
+        <button type="submit" onSubmit={submitContact}>Add contact</button>
+      </form>
+      <ul>Contacts</ul>
     </div>
   );
-};
+}
+}
