cfdc42846b9abb611ef0b5ac3fdab0551e596de5
AlinaBielska <alinkabelka5@gmail.com>
2023-05-24 22:59:48 +0200

    Krok 2 - done (bez css)
    do form dodany dodatkowy input który zapisuje numer telefonu. Contacts renderuje imie i numer kontaktu.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,48 +4,66 @@ import React, { Component } from "react";
 export class App extends Component { 
   state = {
     contacts: [],
-    name: ''
+    name: '',
+    number: ''
   }
 
-  onInputChange = evt => {
+  onInputChangeName = evt => {
     this.setState({ name: evt.target.value });
+  }
+  onInputChangeNumber = evt => {
+    this.setState({ number: evt.target.value });
   }
   onSubmitContact = evt => {
     evt.preventDefault();
-    const { name, contacts } = this.state;
-    this.setState({ contacts: [...contacts, name] });
+    const { name, number, contacts } = this.state;
+    this.setState({ contacts: [...contacts, { name, number }] });
   }
 
   render() {
     const { contacts } = this.state;
     const { name } = this.state;
-    const onInputChange = this.onInputChange;
+    const { number } = this.state;
+    const onInputChangeName = this.onInputChangeName;
+    const onInputChangeNumber = this.onInputChangeNumber;
     const onSubmitContact = this.onSubmitContact;
 
-  return (
-    <div>
-      <form onSubmit={onSubmitContact}>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={onInputChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required />
-        </label>
-        <button type="submit">Add contact</button>
-      </form>
-      <ul>Contacts
-        {contacts.map(contact => {
-          return (
-            <li>{contact}</li>
-          )
-        })}
-      </ul>
-    </div>
-  );
+    return (
+      <div>
+        <form onSubmit={onSubmitContact}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={onInputChangeName}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required />
+          </label>
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={number}
+              onChange={onInputChangeNumber}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <ul>Contacts
+          {contacts.map(contact => {
+            return (
+              <li>{contact.name}: {contact.number}</li>
+            )
+          })}
+        </ul>
+      </div>
+    );
 }
 }
